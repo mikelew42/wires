f475a6c6f076c80ae64541f21be0559e734b61c8
Mike Lewis <mike@Mikes-MacBook-Pro.local>
2016-10-19 01:56:23 -0500

    test stuff

--- Base/TestFramework.js
+++ Base/TestFramework.js
@@ -1,118 +1,340 @@
 var is = require("../is");
 var Base = require("./Base");
 
-var currentBlock;
+var $ = require("jquery");
+
+var $panel;
+$(function(){
+	$panel = $("<div>panel</div>").appendTo("body");
+});
+
+var current;
 
 var Block = Base.extend({
 	name: "Block",
 	init: function(){
-		// console.group("init " + this.name);
-		if (this.root)
-			this.rootBlock = this;
+		if (!this.root)
+			this.root = this;
 
-		this.blocks = {};
-		this.remaining = [];
-		
-		if (this.autoExec)
-			this.exec();
+		this.blocks = {}; // { block.name: block }
+		this.children = []; // [block, block, ...] in order
+		// this.execCount = 0; // ? is this needed?
 
-		// console.groupEnd();
+		this.render();
 	},
-	exec: function(){
-		if (this.finished){
-			console.warn("already finished " + this.name);
-			return false;
-		}
-		var previousCurrentBlock = currentBlock;
-		currentBlock = this;
-
-		console.group(this.name);
-		
-		this.fn();
-
-		var nextParent = this.parent, i = 0;
-
-		// there are more children, and we need to re-exec all from the root down
-		if (this.remaining.length){
-			// console.log("2 or more children, re-exec full stack");
-			// while (nextParent){
-			// 	if (nextParent !== this.rootBlock){
-			// 		i++;
-			// 		nextParent = nextParent.parent;
-			// 		console.groupEnd();
-			// 	}
-			// 	if (i > 15){
-			// 		nextParent = false;
-			// 	}
-
-			// }
-			console.groupEnd();
-			// the rootBlock will store the currentBlock as previousCurrentBlock, and then restore it, using this same function.  
-			currentBlock = this.globalBlock;
-
-			this.currentBlock = this.remaining.shift();
-
-			// the trick will be getting everyone to remember what to do
-			this.rootBlock.exec();
-		} else {
-			// if no more children, this block will lapse, and its parent will have a chance to continue execution.
-			console.info("finished " + this.name);
-			currentBlock = previousCurrentBlock;
-			this.finished = true;
-		}
-
-		console.groupEnd();
+	render: function(){
+		this.$el = $("<div>").addClass("block").text(this.name);
+		this.$children = $("<div>").addClass("children").appendTo(this.$el);
+		if (this.parent && this.parent.$el)
+			this.$el.appendTo(this.parent.$el);
+		else 
+			this.$el.appendTo($panel);
 	},
 	add: function(name, fn){
-		var block;
-		if (name in this.blocks){
-			block = this.blocks[name];
-			if (block.finished)
-				return false;
-			if (!this.currentBlock || (block === this.currentBlock))
+		var block = this.blocks[name],
+			newBlock;
+
+		// has this block been registered?
+		if (block){
+			// is it next to be .exec()?
+			if (block === this.next){
+				// let's do it
 				block.exec();
-			else
+			} else {
+				// it's probably .finished
 				return false;
+			}
+		// nope, make a new one
 		} else {
-			this.blocks[name] = new Block({
+			newBlock = new Block({
 				name: name,
 				fn: fn,
 				parent: this,
-				rootBlock: this.rootBlock,
-				autoExec: !this.foundFirstChild // auto exec the first child
+				root: this.root
 			});
 
-			if (!this.foundFirstChild)
-				this.foundFirstChild = true;
-			else if (this.foundFirstChild)
-				this.remaining.push(this.blocks[name]);
-		}
-	}
-});
+			// store by name
+			this.blocks[name] = newBlock;
 
-var GlobalBlock = Base.extend({
-	name: "GlobalBlock",
+			// add to the ordered array, and add the index to the block
+			newBlock.index = this.children.push(this.blocks[name]) - 1;
+
+			// run first child immediately
+			if (newBlock.index === 0)
+				newBlock.exec();
+		}
+	},
+	isRoot: function(){
+		return this === this.root;
+	},
+	openLogGroup: function(){
+		this.logGroupOpen = true;
+		console.group(this.name);
+	},
+	closeLogGroup: function(){
+		if (this.logGroupOpen){
+			this.logGroupOpen = false;
+			console.groupEnd();
+		}
+	},
+	capture: function(){
+		this.previous = current;
+		current = this;
+	},
+	restore: function(){
+		current = this.previous;
+	},
+	reExec: function(){
+		this.reRun();
+		this.findNext();
+		this.conclude();
+	},
+	reRun: function(){
+		this.capture();
+		this.fn();
+		this.restore();
+	},
+	run: function(suppressLog){
+		this.capture();
+
+		if (this.index === 0 || this.isRoot() )
+			this.openLogGroup();
+		
+		// run the test
+		this.fn();
+
+		// keep root group open
+		if (this.index === 0)
+			this.closeLogGroup();
+
+		this.restore();
+	},
+	findNext: function(){
+		this.next = false;
+		for (var i = 0; i < this.children.length; i++){
+			if (!this.children[i].finished){
+				this.next = this.children[i];
+				break; // find the first unfinished child
+			}
+		}
+	},
+	conclude: function(){
+		if (this.next){
+			// first children have already been logged
+			if (this.next.index !== 0){
+				this.next.openLogGroup;
+			}
+
+			if (this.isRoot())
+				this.reExec();
+		} else {
+			this.finished = true;
+			this.closeLogGroup();
+		}
+	},
+	exec: function(){
+		this.run();
+		this.findNext();
+		this.conclude();
+	},
+	rerun: function(){} // override point for the rootblock below
+});
+/*
+The RootBlocks need to loop through all their kids before ending...
+
+Before, it was find next and then re-exec root.  When we're in the root, we just want to call exec?  or call run...
+
+*/
+
+
+var RootBlock = Block.extend({
+	rerun: function(){}
+})
+
+current = new Block({
+	name: "global",
 	init: function(){
 		this.rootBlocks = [];
 	},
 	add: function(name, fn){
-		this.rootBlocks.push(new Block({
+		var block = new Block({
 			name: name,
-			fn: fn,
-			root: true,
-			globalBlock: this,
-			autoExec: true 
-		}));
+			fn: fn
+		});
+
+		block.exec();
 	}
 });
 
-var globalBlock = currentBlock = new GlobalBlock({
-	name: "globalBlock"
-});
-
-var test = exports.test = function(name, fn){
-	return currentBlock.add.apply(currentBlock, arguments);
+var test = exports.test = function(){
+	return current.add.apply(current, arguments);
 };
+
+// var currentBlock;
+
+// var RootBlock = Block2.extend({
+// 	root: true
+// });
+
+// var Block = Base.extend({
+// 	debug: true,
+// 	verbose: true,
+// 	name: "Block",
+// 	init: function(){
+// 		// console.group("init " + this.name);
+// 		if (this.root)
+// 			this.rootBlock = this;
+
+// 		this.blocks = {};
+// 		this.remaining = [];
+// 		this.execCount = 0;
+		
+// 		if (this.autoExec){
+// 			this.verbose && console.log(".init and .autoExec: " + this.name);
+// 			this.logExec = true;
+// 			this.exec();
+// 		} else {
+// 			this.logExec = false;
+// 			this.verbose && console.log("init and skip: " + this.name);
+// 		}
+
+// 		// console.groupEnd();
+// 	},
+// 	reExec: function(){
+// 		var previousCurrentBlock = currentBlock;
+// 		currentBlock = this;
+
+// 		this.debug && console.warn("Re-running: " + this.name);
+// 		this.fn();
+// 		this.debug && console.warn("Finished re-running: " + this.name);
+
+// 		currentBlock = previousCurrentBlock;
+// 	},
+// 	execNextBlock: function(){
+// 		// in order to exec the next child, we need to reExec all the parent blocks
+
+// 		// when the rootBlock gets reExec(), the global "currentBlock" needs to be the global block....
+// 		currentBlock = globalBlock;
+
+// 		this.nextChild = this.remaining.shift();
+// 		this.verbose && console.info("Next block: " + this.nextChild.name);
+
+// 		// console.log(this.name + " .nextChild: " + this.nextChild.name);
+
+// 		// console.info("this.rootBlock.reExec()");
+// 		// the trick will be getting everyone to remember what to do
+// 		this.verbose && console.debug("Pre-emptive grouping..");
+// 		console.group(this.nextChild.name);
+// 		this.rootBlock.reExec();
+// 		console.groupEnd();
+// 	},
+// 	exec: function(){
+// 		// switch to .reExec after first .exec
+// 		if (this.execCount > 0){
+// 			return this.reExec();
+// 		} else {
+// 			this.execCount++;
+// 		}
+// 		var previousCurrentBlock = currentBlock;
+// 		currentBlock = this;
+
+// 		this.logExec && console.group(this.name);
+		
+// 		// this is where the magic happens... where the blocks are re"added", and are checked if they're the "currentBlock".
+
+// 		this.debug && console.warn("Running: " + this.name);
+// 		this.firstPassComplete = true;
+// 		this.fn();
+
+// 		this.debug && console.warn("Finished running: " + this.name);
+
+// 		// var nextParent = this.parent, i = 0;
+
+// 		// if there is more than 1 child, then they're added to "remaining"
+// 		var childLimit = 20, count = 0;
+// 		if (this.remaining.length){
+// 			while (this.remaining.length){
+// 				count++;
+// 				if (count > childLimit)
+// 					break;
+// 				this.verbose && console.debug(this.remaining.length + " remaining blocks for: " + this.name);
+// 				this.execNextBlock();
+// 			}
+
+// 		} 
+
+// 		// if no more children, this block will lapse, and its parent will have a chance to continue execution.
+// 		this.verbose && console.info("No remaining blocks, finished: " + this.name);
+// 		currentBlock = previousCurrentBlock;
+// 		this.finished = true;
+
+// 		// console.log("blocks for: " + this.name, this.blocks);
+
+// 		this.logExec && console.groupEnd();
+// 	},
+// 	add: function(name, fn){
+// 		var block;
+
+// 		// block has already been registered
+// 		if (name in this.blocks){
+// 			block = this.blocks[name];
+
+// 			// if it has completed, and if children, so have they
+// 			if (block.finished){
+// 				this.verbose && console.info("Re-adding already finished block, skipping: " + block.name);
+// 				return false;
+// 			}
+// 			if (block === this.nextChild){
+// 				if (block.firstPassComplete){
+// 					this.verbose && console.debug("Found this.nextChild, secondary pass, reExec(): ", block.name);
+// 					block.reExec();
+// 				} else {
+// 					this.verbose && console.debug("Found this.nextChild, first pass, exec(): ", block.name);
+// 					block.exec();
+// 				}
+// 			} else {
+// 				return false;
+// 			}
+// 		} else {
+// 			this.verbose && console.info("Adding new block: \"" + name + "\" to: " + this.name);
+// 			this.blocks[name] = new Block({
+// 				name: name,
+// 				fn: fn,
+// 				parent: this,
+// 				rootBlock: this.rootBlock,
+// 				autoExec: !this.foundFirstChild // auto exec the first child
+// 			});
+
+// 			if (!this.foundFirstChild)
+// 				this.foundFirstChild = true;
+// 			else if (this.foundFirstChild)
+// 				this.remaining.push(this.blocks[name]);
+// 		}
+// 	}
+// });
+
+// var GlobalBlock = Base.extend({
+// 	name: "GlobalBlock",
+// 	init: function(){
+// 		this.rootBlocks = [];
+// 	},
+// 	add: function(name, fn){
+// 		this.rootBlocks.push(new Block({
+// 			name: name,
+// 			fn: fn,
+// 			root: true,
+// 			autoExec: true 
+// 		}));
+// 	}
+// });
+
+// var globalBlock = currentBlock = new GlobalBlock({
+// 	name: "globalBlock"
+// });
+
+// var test = exports.test = function(name, fn){
+// 	return currentBlock.add.apply(currentBlock, arguments);
+// };
 
 
 // var describe = exports.describe = function(name, fn){
--- Base/TestFramework.tests.js
+++ Base/TestFramework.tests.js
@@ -1,5 +1,6 @@
 var TestFramework = require("./TestFramework");
 var test = TestFramework.test;
+var $ = require("jquery");
 
 expect = function(a){
 	return {
@@ -9,35 +10,71 @@ expect = function(a){
 	}
 };
 
-test("Root level describe block", function(){
-	var obj = {};
+$(function(){
+	debugger;
+	test("root", function(){
+		console.log("root starter");
+		// console.log("inside Root level describe block");
+		var obj = {};
 
-	test("should be an object", function(){
-		expect(typeof obj).toBe("object");
-	});
-
-	test("should be an instance of an object", function(){
-		expect(obj instanceof Object).toBe(true);
-	});
-
-	test("First level describe block", function(){
-		obj.prop = 5;
-
-		test("should have a prop equal to 5", function(){
-			expect(obj.prop).toBe(5);
+		test("A", function(){
+			console.log("inside A");
 		});
 
-		test("Second level describe block", function(){
-			obj.prop += 5;
+		console.log("A B tweener");
 
-			test("should increase by 5", function(){
-				expect(obj.prop).toBe(10);
-			});
-
-			test("should increase by another 5", function(){
-				obj.prop += 5;
-				expect(obj.prop).toBe(15);
-			});
+		test("B", function(){
+			console.log("inside B");
 		});
-	});
+	 
+	 // 	test("Z", function(){
+
+	 // 		test("Z1", function(){
+
+	 // 			test("Z1a", function(){});
+
+	 // 			test("Z1b", function(){});
+	 // 		});
+
+	 // 		test("Z2", function(){
+	 // 			test("Z2a", function(){});
+
+	 // 			test("Z2b", function(){});
+	 // 		});
+	 // 	});
+		// console.log("B C tweener");
+
+		// test("C", function(){
+		// 	console.log("C starter");
+		// 	obj.prop = 5;
+
+		// 	test("C1", function(){
+		// 		expect(obj.prop).toBe(5);
+		// 	});
+
+		// 	console.log("C1 C2 tweener");
+
+		// 	test("C2", function(){
+		// 		console.log("C2 starter");
+		// 		obj.prop += 5;
+
+		// 		test("C2a", function(){
+		// 			expect(obj.prop).toBe(10);
+		// 		});
+
+		// 		console.log("C2a C2b tweener");
+
+		// 		test("C2b", function(){
+		// 			obj.prop += 5;
+		// 			expect(obj.prop).toBe(15);
+		// 		});
+
+		// 		console.log("C2 ender");
+		// 	});
+
+		// 	console.log("C ender");
+		// });
+
+		console.log("root ender");
+	});	
 });
