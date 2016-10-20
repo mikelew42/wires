9bc83c0df3dcaaa910c74519b517980365cfd0c2
Mike Lewis <mike@Mikes-MacBook-Pro.local>
2016-10-19 22:12:51 -0500

    testing stuff

--- Base/TestFramework.tests.js
+++ Base/TestFramework.tests.js
@@ -1,6 +1,7 @@
 var TestFramework = require("./TestFramework");
 var test = TestFramework.test;
 var $ = require("jquery");
+var ValidationTest = require("./ValidationTest");
 
 expect = function(a){
 	return {
@@ -10,71 +11,137 @@ expect = function(a){
 	}
 };
 
+var index = 0, IDs = [
+	1, 2, 3, 4, 5, // A
+	1, 3, 6, 7, 8, 9, 10, 4, 5, // B1
+	1, 3, 6, 8, 11, 9, 10, 4, 5, // B2
+	1, 3, 6, 8, 9, 12, 10, 4, 5, // B3
+	1, 3, 4, 13, 14, 15, 16, 17, 18, 19, 20, 5, // C1a
+	1, 3, 4, 13, 14, 16, 21, 17, 18, 19, 20, 5, // C1b
+	1, 3, 4, 13, 14, 16, 17, 22, 18, 19, 20, 5, // C1c
+	1, 3, 4, 13, 19, 23, 24, 25, 26, 20, 5, // C2a
+	1, 3, 4, 13, 19, 23, 25, 27, 26, 20, 5// C2b
+];
+var next = function(){
+	return IDs[index++];
+};
+
 $(function(){
 	debugger;
 	test("root", function(){
-		console.log("root starter");
-		// console.log("inside Root level describe block");
-		var obj = {};
+		console.log("root.start");
+		console.assert(next() === 1);
 
 		test("A", function(){
-			console.log("inside A");
+			console.log("A.start");
+			console.assert(next() === 2);
 		});
 
-		console.log("A B tweener");
+		console.log("root.A/B");
+		console.assert(next() === 3);
 
 		test("B", function(){
-			console.log("inside B");
+			console.log("B.start");
+			console.assert(next() == 6);
+
+			test("B1", function(){
+				console.log("B1");
+				console.assert(next() === 7);
+			});
+
+			console.log("B.B1/B2");
+			console.assert(next() === 8);
+
+			test("B2", function(){
+				console.log("B2")
+				console.assert(next() === 11);
+			});
+
+			console.log("B.B2/B3");
+			console.assert(next() === 9);
+
+			test("B3", function(){
+				console.log("inside B3");
+				console.assert(next() === 12);
+			});
+
+			console.log("B.end");
+			console.assert(next() === 10);
 		});
 	 
-	 // 	test("Z", function(){
+		console.log("root.B/C");
+		console.assert(next() === 4);
 
-	 // 		test("Z1", function(){
+		test("C", function(){
+			console.log("C.start");
+			console.assert(next() === 13);
 
-	 // 			test("Z1a", function(){});
+			test("C1", function(){
+				console.log("C1.start");
+				console.assert(next() === 14);
 
-	 // 			test("Z1b", function(){});
-	 // 		});
+				test("C1a", function(){
+					console.log("C1a");
+					console.assert(next() === 15);
+				});
 
-	 // 		test("Z2", function(){
-	 // 			test("Z2a", function(){});
+				console.log("C1.C1a/C1b");
+				console.assert(next() === 16);
 
-	 // 			test("Z2b", function(){});
-	 // 		});
-	 // 	});
-		// console.log("B C tweener");
+				test("C1b", function(){
+					console.log("C1b");
+					console.assert(next() === 21);
+				});
 
-		// test("C", function(){
-		// 	console.log("C starter");
-		// 	obj.prop = 5;
+				console.log("C1.C1b/C1c");
+				console.assert(next() === 17);
 
-		// 	test("C1", function(){
-		// 		expect(obj.prop).toBe(5);
-		// 	});
+				test("C1c", function(){
+					console.log("C1c");
+					console.assert(next() === 22);
+				});
 
-		// 	console.log("C1 C2 tweener");
+				console.log("C1.end");
+				console.assert(next() === 18);
+			});
 
-		// 	test("C2", function(){
-		// 		console.log("C2 starter");
-		// 		obj.prop += 5;
+			console.log("C.C1/C2");
+			console.assert(next() === 19);
 
-		// 		test("C2a", function(){
-		// 			expect(obj.prop).toBe(10);
-		// 		});
+			test("C2", function(){
+				console.log("C2.start");
+				console.assert(next() === 23);
 
-		// 		console.log("C2a C2b tweener");
+				test("C2a", function(){
+					console.log("C2a");
+					console.assert(next() === 24);
+				});
 
-		// 		test("C2b", function(){
-		// 			obj.prop += 5;
-		// 			expect(obj.prop).toBe(15);
-		// 		});
+				console.log("C2.C2a/C2b");
+				console.assert(next() === 25);
 
-		// 		console.log("C2 ender");
-		// 	});
+				test("C2b", function(){
+					console.log("C2b");
+					console.assert(next() === 27);
+				});
 
-		// 	console.log("C ender");
-		// });
+				console.log("C2.end");
+				console.assert(next() === 26);
+			});
 
-		console.log("root ender");
+			console.log("C.end");
+			console.assert(next() === 20);
+		});
+
+		console.log("root.end");
+		console.assert(next() === 5);
+
+		test("D", function(){
+
+		});
+
+		test("E", function(){
+			
+		});
 	});	
 });
