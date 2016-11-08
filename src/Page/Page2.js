e159df7564bc37372f88bbe867fc0ec7409e78e0
Mike Lewis <mike@Mikes-MacBook-Pro.local>
2016-11-08 05:42:17 -0600

    stuff

---
+++ src/Page/Page2.js
@@ -0,0 +1,16 @@
+var View = require("../View/View");
+
+var is = require("../is");
+var Route = require("../core/Route");
+var router = require("../router");
+var utils = require("../utils/utils");
+var Base3 = require("../Base3/Base3");
+
+var $ = require("jquery");
+
+
+var Page2 = View.extend({
+	
+});
+
+module.exports = Page2;
--- src/index.entry.js
+++ src/index.entry.js
@@ -1,6 +1,12 @@
 // var app = require("./app");
 require("./test");
+var App = require("./App/App");
 
-var Page = require("./Page/Page");
-// var Item = require("./Item/Item");
-
+var app = new App({
+	// create: function(){
+	// 	this._init(); // before
+	// 	this.init(); // always leave empty for instantiation?
+	// 	this.init_(); // after
+	// }
+	
+});
