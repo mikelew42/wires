9749232b9c0ad9399f78746064e3e92c04f40a93
Mike Lewis <mike@Mikes-MacBook-Pro.local>
2016-11-11 09:28:17 -0600

    hmm

--- src/index.entry.js
+++ src/index.entry.js
@@ -1,16 +1,47 @@
-// var app = require("./app");
-require("./test");
-var App = require("./App/App");
+require("./Lorem");
+// require("test");
+require("./styles.less");
+var Application = require("Application");
 
-console.log("yerp?");
-console.log(require("test"));
-console.log(require("test2"));
 
-var app = new App({
-	// create: function(){
-	// 	this._init(); // before
-	// 	this.init(); // always leave empty for instantiation?
-	// 	this.init_(); // after
-	// }
-	
+var app = new Application({
+	load_pages: function(){
+		var pages = require.context("./root/", true, /\.page\.js$/);
+		// console.log("pages", pages.keys());
+		this.require_all(pages);
+
+		var other_pages = require.context("./pages/", true, /\.page\.js$/);
+		this.require_all(other_pages);
+	},
+	load_tests: function(){
+		var app = this;
+		var tests = require.context("./", true, /\.test\.js$/);
+
+		this.test_routes(tests);
+
+		this.router.addRoutes(new this.Router.Route({
+			pathname: "/test/",
+			label: "Tests"
+		}).then(function(){
+			app.require_all(tests);
+		}));
+	},
+	test_routes: function(requireContext) {
+		var keys = requireContext.keys(), key;
+		for (var i = 0; i < keys.length; i++){
+			key = keys[i].replace("./", "").replace(".tests.js", "");
+
+			this.router.addRoutes(new this.Router.Route({
+				pathname: "test/" + key,
+				label: key,
+				key: keys[i],
+				allowDefault: true,
+				matchBeginning: true
+			}).then(function(){
+				// console.clear();
+				requireContext(this.key);
+			}));
+		}
+		// keys.forEach(context); 
+	}
 });
